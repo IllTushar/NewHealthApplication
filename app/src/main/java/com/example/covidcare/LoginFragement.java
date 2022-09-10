8738439787318d7b66eee7e15bfbae11ec2313e0
Tushar <gtushar7668@gmail.com>
2022-09-10 10:14:10 +0530

    Third Commit

--- app/src/main/java/com/example/covidcare/LoginFragement.java
+++ app/src/main/java/com/example/covidcare/LoginFragement.java
@@ -1,5 +1,6 @@
 package com.example.covidcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class LoginFragement extends Fragment {
 
-
+TextView forgetPassword;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -23,6 +25,13 @@ public class LoginFragement extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root= inflater.inflate(R.layout.fragment_login_fragement, container, false);
+        forgetPassword = root.findViewById(R.id.forgetPassword);
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity().getApplication(),ForgetPassword.class));
+            }
+        });
         return root;
     }
 }
